52e01c36143a79d56d598bf8153eeabfe2a2b546
gaidar.vu <gaidar.vu@gmail.com>
2025-01-28 23:25:37 +0300

    add infrastructure

---
+++ yc-env-terraform/locals.tf
@@ -0,0 +1,8 @@
+locals {
+  cloud_init_configs = {
+    vm1 = file("${path.module}/cloud-init/vm1.yaml")
+    vm2 = file("${path.module}/cloud-init/vm2.yaml")
+    vm3 = file("${path.module}/cloud-init/vm3.yaml")
+    vm4 = file("${path.module}/cloud-init/vm4.yaml")
+  }
+}
